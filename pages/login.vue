0741a2c1cfcf38edb863c161ab835a01d6adf8ee
Patrick Finucane <patrick.finucane@gmail.com>
2020-12-21 21:17:23 -0600

    start

---
+++ pages/login.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div class="max-w-xs mx-auto my-10">
+            <form
+                    class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Email
+                    </label>
+                    <input
+                            v-model="credentials.email"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input
+                            v-model="credentials.password"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password" type="password" placeholder="Password">
+                    <p v-if="false" class="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button
+                            v-on:click="onSubmit"
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Sign In
+                    </button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Forgot Password?
+                    </a>
+                </div>
+            </form>
+            <p class="text-center text-gray-500 text-xs">
+                &copy;2020 Acme Corp. All rights reserved.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: "login.vue",
+    data() {
+      return {
+        credentials: {
+          email: '',
+          password: '',
+        },
+      };
+    },
+    methods: {
+      async onSubmit () {
+        let response = await this.$store.dispatch('login', this.credentials);
+
+        console.log(response);
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+
+const bigCommerce = axios.create({
+  baseURL: `https://store-w125h0h2n4.mybigcommerce.com/graphql`,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: "Bearer ",
+  }
+});
+
+export const state = () => ({
+  userErrors: [],
+  product: {},
+  products: [],
+});
+
+export const mutations = {
+  setErrors (state, userErrors) {
+    state.userErrors = userErrors || []
+  },
+};
+
+export const actions = {
+  async nuxtServerInit(ctx, context) {
+  },
+
+  async login({ state, commit, rootState }, { email, password }) {
+    const response = await bigCommerce.post(null, {
+      query: `
+        mutation Login($email: String!, $password: String!) {
+          login(email: $email, password: $password) {
+            result
+          }
+        }
+      `,
+      variables: { email, password} });
+
+
+  }
+
+};
